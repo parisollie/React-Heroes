8b168def8dc582f472e59509ea3d93d562875467
parisollie <pjff18@gmail.com>
2025-04-07 13:45:14 -0600

    first commit

---
+++ src/HeroesApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeroesApp = () => (
+    <div>
+        <h1>HeroesApp</h1>
+    </div>
+)
+
+export default HeroesApp
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+import HeroesApp from './HeroesApp';
+
+
+
+createRoot(document.getElementById('root')).render(
+
+  <HeroesApp />
+
+);
