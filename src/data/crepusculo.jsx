8cbf819b7ee71c85bfb616e0a2fc5bbe0d788f84
parisollie <pjff18@gmail.com>
2025-04-08 01:44:11 -0600

    crepusculo

---
+++ src/data/crepusculo.jsx
@@ -0,0 +1,185 @@
+export const twilightCharacters = [
+    // PROTAGONISTAS (vp-)
+    {
+        'id': 'vp-bella',
+        'name': 'Bella Swan',
+        'affiliation': 'Cullen/Humanos',
+        'role': 'Protagonista',
+        'first_appearance': 'Crepúsculo (2005)',
+        'species': 'Humana (luego vampira)',
+        'alias': 'Isabella Swan-Cullen'
+    },
+    {
+        'id': 'vp-edward',
+        'name': 'Edward Cullen',
+        'affiliation': 'Familia Cullen',
+        'role': 'Vampiro enamorado de Bella',
+        'first_appearance': 'Crepúsculo (2005)',
+        'species': 'Vampiro',
+        'alias': 'Edward Anthony Masen'
+    },
+    {
+        'id': 'vp-jacob',
+        'name': 'Jacob Black',
+        'affiliation': 'Manada de La Push',
+        'role': 'Lobizón y mejor amigo de Bella',
+        'first_appearance': 'Crepúsculo (2005)',
+        'species': 'Lobizón',
+        'alias': 'Jake'
+    },
+    {
+        'id': 'vp-carlisle',
+        'name': 'Carlisle Cullen',
+        'affiliation': 'Familia Cullen',
+        'role': 'Líder y padre de la familia',
+        'first_appearance': 'Crepúsculo (2005)',
+        'species': 'Vampiro',
+        'alias': 'El Médico Vampiro'
+    },
+    {
+        'id': 'vp-esme',
+        'name': 'Esme Cullen',
+        'affiliation': 'Familia Cullen',
+        'role': 'Madre de la familia',
+        'first_appearance': 'Crepúsculo (2005)',
+        'species': 'Vampiro',
+        'alias': 'El Corazón de los Cullen'
+    },
+    {
+        'id': 'vp-alice',
+        'name': 'Alice Cullen',
+        'affiliation': 'Familia Cullen',
+        'role': 'Vidente y hermana de Edward',
+        'first_appearance': 'Crepúsculo (2005)',
+        'species': 'Vampiro',
+        'alias': 'La Clarividente'
+    },
+    {
+        'id': 'vp-jasper',
+        'name': 'Jasper Hale',
+        'affiliation': 'Familia Cullen',
+        'role': 'Experto en manipulación emocional',
+        'first_appearance': 'Crepúsculo (2005)',
+        'species': 'Vampiro',
+        'alias': 'El Soldado'
+    },
+    {
+        'id': 'vp-rosalie',
+        'name': 'Rosalie Hale',
+        'affiliation': 'Familia Cullen',
+        'role': 'Hermana de Edward',
+        'first_appearance': 'Crepúsculo (2005)',
+        'species': 'Vampiro',
+        'alias': 'La Bella Inmortal'
+    },
+    {
+        'id': 'vp-charlie',
+        'name': 'Charlie Swan',
+        'affiliation': 'Humanos',
+        'role': 'Padre de Bella',
+        'first_appearance': 'Crepúsculo (2005)',
+        'species': 'Humano',
+        'alias': 'El Sheriff de Forks'
+    },
+    {
+        'id': 'vp-renesmee',
+        'name': 'Renesmee Cullen',
+        'affiliation': 'Familia Cullen',
+        'role': 'Hija de Bella y Edward',
+        'first_appearance': 'Amanecer (2008)',
+        'species': 'Híbrida (vampira-humana)',
+        'alias': 'Nessie'
+    },
+
+    // ANTAGONISTAS (vi-)
+    {
+        'id': 'vi-victoria',
+        'name': 'Victoria',
+        'affiliation': 'Vampiros Nómadas',
+        'role': 'Vengadora de James',
+        'first_appearance': 'Crepúsculo (2005)',
+        'species': 'Vampiro',
+        'alias': 'La Cazadora Roja'
+    },
+    {
+        'id': 'vi-james',
+        'name': 'James',
+        'affiliation': 'Vampiros Nómadas',
+        'role': 'Cazador de Bella',
+        'first_appearance': 'Crepúsculo (2005)',
+        'species': 'Vampiro',
+        'alias': 'El Rastreador'
+    },
+    {
+        'id': 'vi-laurent',
+        'name': 'Laurent',
+        'affiliation': 'Vampiros Nómadas',
+        'role': 'Ex-aliado de los Cullen',
+        'first_appearance': 'Crepúsculo (2005)',
+        'species': 'Vampiro',
+        'alias': 'El Traidor'
+    },
+    {
+        'id': 'vi-aro',
+        'name': 'Aro',
+        'affiliation': 'Guardia Volturi',
+        'role': 'Líder de los Volturi',
+        'first_appearance': 'Luna Nueva (2006)',
+        'species': 'Vampiro',
+        'alias': 'El Juez Despiadado'
+    },
+    {
+        'id': 'vi-caius',
+        'name': 'Caius',
+        'affiliation': 'Guardia Volturi',
+        'role': 'Miembro de los Volturi',
+        'first_appearance': 'Luna Nueva (2006)',
+        'species': 'Vampiro',
+        'alias': 'El Sádico'
+    },
+    {
+        'id': 'vi-marcus',
+        'name': 'Marcus',
+        'affiliation': 'Guardia Volturi',
+        'role': 'Miembro de los Volturi',
+        'first_appearance': 'Luna Nueva (2006)',
+        'species': 'Vampiro',
+        'alias': 'El Indiferente'
+    },
+    {
+        'id': 'vi-jane',
+        'name': 'Jane',
+        'affiliation': 'Guardia Volturi',
+        'role': 'Miembro con poder de dolor ilusorio',
+        'first_appearance': 'Luna Nueva (2006)',
+        'species': 'Vampiro',
+        'alias': 'La Torturadora'
+    },
+    {
+        'id': 'vi-alec',
+        'name': 'Alec',
+        'affiliation': 'Guardia Volturi',
+        'role': 'Gemelo de Jane',
+        'first_appearance': 'Luna Nueva (2006)',
+        'species': 'Vampiro',
+        'alias': 'El Privador de Sentidos'
+    },
+    {
+        'id': 'vi-demetri',
+        'name': 'Demetri',
+        'affiliation': 'Guardia Volturi',
+        'role': 'Rastreador imparable',
+        'first_appearance': 'Luna Nueva (2006)',
+        'species': 'Vampiro',
+        'alias': 'El Perseguidor'
+    },
+    {
+        'id': 'vi-riley',
+        'name': 'Riley Biers',
+        'affiliation': 'Ejército de Recién Nacidos',
+        'role': 'Líder del ejército de recién convertidos',
+        'first_appearance': 'Eclipse (2007)',
+        'species': 'Vampiro (Recién Nacido)',
+        'alias': 'El Segundo al Mando de Victoria'
+    }
+];
---
+++ src/data/hp.jsx
@@ -0,0 +1,185 @@
+export const harryPotterCharacters = [
+    // HÉROES (Aliados de Harry Potter)
+    {
+        'id': 'hp-potter',
+        'name': 'Harry Potter',
+        'affiliation': 'Orden del Fénix',
+        'role': 'El Elegido',
+        'first_appearance': 'Harry Potter y la piedra filosofal (1997)',
+        'house': 'Gryffindor',
+        'alias': 'El Niño que Vivió'
+    },
+    {
+        'id': 'hp-granger',
+        'name': 'Hermione Granger',
+        'affiliation': 'Orden del Fénix',
+        'role': 'Bruja brillante',
+        'first_appearance': 'Harry Potter y la piedra filosofal (1997)',
+        'house': 'Gryffindor',
+        'alias': 'La más inteligente de su generación'
+    },
+    {
+        'id': 'hp-weasley',
+        'name': 'Ron Weasley',
+        'affiliation': 'Orden del Fénix',
+        'role': 'Mejor amigo de Harry',
+        'first_appearance': 'Harry Potter y la piedra filosofal (1997)',
+        'house': 'Gryffindor',
+        'alias': 'El Rey del Ajedrez Mágico'
+    },
+    {
+        'id': 'hp-dumbledore',
+        'name': 'Albus Dumbledore',
+        'affiliation': 'Orden del Fénix',
+        'role': 'Director de Hogwarts',
+        'first_appearance': 'Harry Potter y la piedra filosofal (1997)',
+        'house': 'Gryffindor',
+        'alias': 'El Mayor Mago de Todos los Tiempos'
+    },
+    {
+        'id': 'hp-lupin',
+        'name': 'Remus Lupin',
+        'affiliation': 'Orden del Fénix',
+        'role': 'Profesor de Defensa Contra las Artes Oscuras',
+        'first_appearance': 'Harry Potter y el prisionero de Azkaban (1999)',
+        'house': 'Gryffindor',
+        'alias': 'Hombre Lobo'
+    },
+    {
+        'id': 'hp-sirius',
+        'name': 'Sirius Black',
+        'affiliation': 'Orden del Fénix',
+        'role': 'Padrino de Harry',
+        'first_appearance': 'Harry Potter y el prisionero de Azkaban (1999)',
+        'house': 'Gryffindor',
+        'alias': 'Canuto'
+    },
+    {
+        'id': 'hp-neville',
+        'name': 'Neville Longbottom',
+        'affiliation': 'Ejército de Dumbledore',
+        'role': 'Miembro clave en la batalla final',
+        'first_appearance': 'Harry Potter y la piedra filosofal (1997)',
+        'house': 'Gryffindor',
+        'alias': 'El Destructor de Horrocruxes'
+    },
+    {
+        'id': 'hp-luna',
+        'name': 'Luna Lovegood',
+        'affiliation': 'Ejército de Dumbledore',
+        'role': 'Amiga excéntrica',
+        'first_appearance': 'Harry Potter y la Orden del Fénix (2003)',
+        'house': 'Ravenclaw',
+        'alias': 'Loony Lovegood'
+    },
+    {
+        'id': 'hp-ginny',
+        'name': 'Ginny Weasley',
+        'affiliation': 'Ejército de Dumbledore',
+        'role': 'Bruja talentosa',
+        'first_appearance': 'Harry Potter y la piedra filosofal (1997)',
+        'house': 'Gryffindor',
+        'alias': 'La Chica que Venció a Riddle'
+    },
+    {
+        'id': 'hp-hagrid',
+        'name': 'Rubeus Hagrid',
+        'affiliation': 'Hogwarts',
+        'role': 'Guardián de las Llaves',
+        'first_appearance': 'Harry Potter y la piedra filosofal (1997)',
+        'house': 'Gryffindor (expulsado)',
+        'alias': 'El Medio Gigante'
+    },
+
+    // VILLANOS (Mortífagos)
+    {
+        'id': 'mt-voldemort',
+        'name': 'Lord Voldemort',
+        'affiliation': 'Mortífagos',
+        'role': 'Señor Oscuro',
+        'first_appearance': 'Harry Potter y la piedra filosofal (1997)',
+        'house': 'Slytherin',
+        'alias': 'Tom Riddle'
+    },
+    {
+        'id': 'mt-bellatrix',
+        'name': 'Bellatrix Lestrange',
+        'affiliation': 'Mortífagos',
+        'role': 'Mano derecha de Voldemort',
+        'first_appearance': 'Harry Potter y la Orden del Fénix (2003)',
+        'house': 'Slytherin',
+        'alias': 'La Más Cruel'
+    },
+    {
+        'id': 'mt-lucius',
+        'name': 'Lucius Malfoy',
+        'affiliation': 'Mortífagos',
+        'role': 'Patriarca de los Malfoy',
+        'first_appearance': 'Harry Potter y la cámara secreta (1998)',
+        'house': 'Slytherin',
+        'alias': 'El Purista de Sangre'
+    },
+    {
+        'id': 'mt-dolores',
+        'name': 'Dolores Umbridge',
+        'affiliation': 'Ministerio de Magia',
+        'role': 'Inquisidora de Hogwarts',
+        'first_appearance': 'Harry Potter y la Orden del Fénix (2003)',
+        'house': 'Slytherin',
+        'alias': 'La Rana'
+    },
+    {
+        'id': 'mt-snape',
+        'name': 'Severus Snape',
+        'affiliation': 'Mortífagos (espía doble)',
+        'role': 'Profesor de Pociones',
+        'first_appearance': 'Harry Potter y la piedra filosofal (1997)',
+        'house': 'Slytherin',
+        'alias': 'El Príncipe Mestizo'
+    },
+    {
+        'id': 'mt-peter',
+        'name': 'Peter Pettigrew',
+        'affiliation': 'Mortífagos',
+        'role': 'Traidor de los Potter',
+        'first_appearance': 'Harry Potter y el prisionero de Azkaban (1999)',
+        'house': 'Gryffindor',
+        'alias': 'Colagusano'
+    },
+    {
+        'id': 'mt-barty',
+        'name': 'Barty Crouch Jr.',
+        'affiliation': 'Mortífagos',
+        'role': 'Impostor de Moody',
+        'first_appearance': 'Harry Potter y el cáliz de fuego (2000)',
+        'house': 'Desconocido',
+        'alias': 'El Falso Ojo-Loco'
+    },
+    {
+        'id': 'mt-greyback',
+        'name': 'Fenrir Greyback',
+        'affiliation': 'Mortífagos',
+        'role': 'Lobhombre asesino',
+        'first_appearance': 'Harry Potter y el misterio del príncipe (2005)',
+        'house': 'Ninguna',
+        'alias': 'El Devorador de Niños'
+    },
+    {
+        'id': 'mt-narcissa',
+        'name': 'Narcissa Malfoy',
+        'affiliation': 'Familia Malfoy',
+        'role': 'Madre de Draco',
+        'first_appearance': 'Harry Potter y el misterio del príncipe (2005)',
+        'house': 'Slytherin',
+        'alias': 'La Salvadora de Harry'
+    },
+    {
+        'id': 'mt-draco',
+        'name': 'Draco Malfoy',
+        'affiliation': 'Mortífagos (forzado)',
+        'role': 'Estudiante rival',
+        'first_appearance': 'Harry Potter y la piedra filosofal (1997)',
+        'house': 'Slytherin',
+        'alias': 'El Niño de Oro de Slytherin'
+    }
+];
